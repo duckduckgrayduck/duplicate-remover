fee7c0c9ba707aa73649b432f2a75a46628f4897
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2024-07-19 14:52:01 -0500

    Add delete operation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ DocumentCloud Add-On that removes duplicates by their hash value
 import csv
 import sys
 from documentcloud.addon import AddOn
+from documentcloud.exceptions import APIError
 
 class DuplicateRemover(AddOn):
     """An example Add-On for DocumentCloud."""
@@ -22,17 +23,27 @@ class DuplicateRemover(AddOn):
             file_hash = document.file_hash
             if file_hash in known_hashes:
                 # If file_hash is already known, add it to to_delete
-                to_delete.append({
-                    'deleted_id': document.id,
-                    'reason': f"Deleted because it has the same hash as {known_hashes[file_hash]}"
-                })
+                try:
+                    document.delete()
+                    to_delete.append({
+                        'deleted_id': document.id,
+                        'reason': f"Deleted because it has the same hash as document with id {known_hashes[file_hash]}"
+                    })
+                except APIError:
+                    self.set_message(
+                        "Could not delete some documents due to insufficient permissions." 
+                        "You can only delete documents you own."
+                    )
+                    to_delete.append({
+                        'deleted_id': document.id,
+                        'reason': "Failed to delete due to insufficient permissions"
+                    })
+
             else:
                 # Otherwise, add file_hash and document id to known_hashes dictionary
                 known_hashes[file_hash] = document.id
         # Print the list of deletions with reasons
         # print(known_hashes)
-        for deletion in to_delete:
-            print(f"Deleted document {deletion['deleted_id']} - {deletion['reason']}")
 
         csv_filename = "deleted_documents.csv"
         with open(csv_filename, mode='w', newline='', encoding='utf-8') as csvfile:
