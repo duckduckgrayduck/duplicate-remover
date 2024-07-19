25353dc8386ee1bbf392b9dd50f3856c7e99c486
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2024-07-19 14:45:25 -0500

    Add csv option

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 """
 DocumentCloud Add-On that removes duplicates by their hash value
 """
-
+import csv
+import sys
 from documentcloud.addon import AddOn
-
 
 class DuplicateRemover(AddOn):
     """An example Add-On for DocumentCloud."""
 
     def main(self):
         """The main add-on functionality goes here."""
-
+        confirm = self.data.get("confirm", False)
+        if confirm is False:
+            self.set_message("You did not check the confirmation box to run this Add-On")
+            sys.exit(0)
         known_hashes = {}
         to_delete = []
 
@@ -27,10 +30,18 @@ class DuplicateRemover(AddOn):
                 # Otherwise, add file_hash and document id to known_hashes dictionary
                 known_hashes[file_hash] = document.id
         # Print the list of deletions with reasons
-        print(known_hashes)
+        # print(known_hashes)
         for deletion in to_delete:
             print(f"Deleted document {deletion['deleted_id']} - {deletion['reason']}")
 
+        csv_filename = "deleted_documents.csv"
+        with open(csv_filename, mode='w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['deleted_id', 'reason']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for deletion in to_delete:
+                writer.writerow(deletion)
+        self.upload_file(open(csv_filename, encoding='utf-8'))
 
 if __name__ == "__main__":
     DuplicateRemover().main()
